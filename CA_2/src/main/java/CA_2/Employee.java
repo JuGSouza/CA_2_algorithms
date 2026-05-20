8ddee407c2087edf64e2bcb3008b861036bc8f57
JuGSouza <jgs.contact55@gmail.com>
2026-05-20 18:11:56 +0100

    adding Employee

---
+++ CA_2/src/main/java/CA_2/Employee.java
@@ -0,0 +1,31 @@
+package CA_2;
+
+public class Employee {
+
+    private String name;
+    private String managerType;
+    private String department;
+
+    public Employee(String name, String managerType, String department) {
+        this.name = name;
+        this.managerType = managerType;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getManagerType() {
+        return managerType;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return name + " | Manager Type: " + managerType + " | Department: " + department;
+    }
+}
