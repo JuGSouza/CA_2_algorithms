30395dfae062c6482a7476120f1908123e5960bc
JuGSouza <jgs.contact55@gmail.com>
2026-05-20 18:48:02 +0100

    adding department

---
+++ CA_2/src/main/java/CA_2/Department.java
@@ -0,0 +1,14 @@
+package CA_2;
+
+public class Department {
+
+    private String departmentName;
+
+    public Department(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+}
