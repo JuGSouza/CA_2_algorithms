3ce1ddda23d5c4965e66db7177b7a9aa2eac644f
JuGSouza <jgs.contact55@gmail.com>
2026-05-20 19:39:57 +0100

    creating option

---
+++ CA_2/src/main/java/CA_2/MenuOption.java
@@ -0,0 +1,11 @@
+package CA_2;
+
+// Enum used for the main menu options.
+// The CA2 asks to use enums for the menu.
+public enum MenuOption {
+    SORT,
+    SEARCH,
+    ADD_RECORD,
+    CREATE_TREE,
+    EXIT
+}
