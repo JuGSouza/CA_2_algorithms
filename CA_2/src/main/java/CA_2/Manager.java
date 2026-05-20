a5901760d48f262c685ebd10730a4b1a8ea82abd
JuGSouza <jgs.contact55@gmail.com>
2026-05-20 19:06:58 +0100

    adding manager class

---
+++ CA_2/src/main/java/CA_2/Manager.java
@@ -0,0 +1,15 @@
+package CA_2;
+
+
+public class Manager {
+
+    private String managerType;
+
+    public Manager(String managerType) {
+        this.managerType = managerType;
+    }
+
+    public String getManagerType() {
+        return managerType;
+    }
+}
